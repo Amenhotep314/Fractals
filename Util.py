ea0828b809d687be9d28c03e813f2f155eba3ad5
Amenhotep314 <nathan.lenzini@gmail.com>
2020-12-19 09:45:21 -0500

    Implements get_number UI function

--- Util.py
+++ Util.py
@@ -1,7 +1,7 @@
 def get_choice(options, prompt):
 
     while True:
-        print(prompt + "(1 - " + str(len(options)) + "):")
+        print(prompt + " (1 - " + str(len(options)) + "):")
         for i, option in enumerate(options):
             print(str(i + 1) + '. ' + option)
         choice = input('>>> ')
@@ -14,3 +14,20 @@ def get_choice(options, prompt):
                 print("That's not one of the choices.")
         except ValueError:
             print("Please enter a number.")
+
+
+def get_number(prompt, bound=0, require_positive=True):
+
+    while True:
+        print(prompt)
+        choice = input('>>> ')
+
+        try:
+            choice = int(choice)
+            if bound != 0 and choice > bound:
+                print("That value is too high.")
+            if require_positive and choice < 0:
+                print("Your input must be positive.")
+        except ValueError:
+            print("Please enter a number.")
+
