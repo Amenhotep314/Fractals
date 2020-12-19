ac81127e552afd8a2ada809681058f35d272e050
Amenhotep314 <nathan.lenzini@gmail.com>
2020-12-19 09:25:02 -0500

    Outlines functions to be called from main

--- Julia_Set.py
+++ Julia_Set.py
@@ -1,17 +1,26 @@
 from tkinter import Tk, Canvas
 from random import randint
 
+from Text_Wrapper import Text_Wrapper_Reader, get_file
+from Util import get_choice
+
 
 def main():
 
-    for i in range(width):
-        for j in range(height):
-            fill = random_color()
-            print(fill + "\t" + str(i) + ' x ' + str(j))        
+    print("Welcome to Julia Set Generator/Renderer")
+    choice = get_choice(["Generate a set", "Render a set", "Help!"], "What would you like to do?")
+
+    if choice == 0:
+        generate_set()
+    elif choice == 1:
+        render_set()
+    else:
+        help()
 
 
-def render_set(data):
+def render_set():
 
+    data = Text_Wrapper_Reader(get_file())
     window = Tk()
     canvas = Canvas(window, width=data.width, height=data.height)
     canvas.pack()
@@ -24,6 +33,18 @@ def render_set(data):
         canvas.create_rectangle(x, y, x+res, y+res, fill='#FF00FF', outline='')
 
     window.mainloop()
+
+
+def help():
+
+    print("JULIA SETS")
+    print("Information about Julia Sets goes here.\n")
+
+    print("GENERATING SETS")
+    print("Information about generating sets goes here.\n")
+
+    print("RENDERING SETS")
+    print("Information about rendering sets goes here.\n")
 
 
 if __name__ == "__main__":
