807ac70cd364298c486af998b2e8f23235c90204
Amenhotep314 <nathan.lenzini@gmail.com>
2021-04-14 19:53:02 -0400

    Adds basics of a Java implementation

---
+++ Java/Fractals.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Fractals
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\nWelcome to Fractal Set Generator & Renderer");
+
+        while(true)
+        {
+            
+        }
+    }
+}
